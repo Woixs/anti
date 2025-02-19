862470ec7f53602b57712e958f785f079b9afaec
Woixs <zymbacklinker@gmail.com>
2025-02-19 15:57:41 +0300

    Update in.js

--- in.js
+++ in.js
@@ -1,10 +1,10 @@
-// START link_privnews123
-(function() {
-var a = document.createElement('a');
-a.href = 'https://privnews.com/';
-a.title = 'Hack Haber';
-a.textContent = 'Hack Haber';
-a.style = 'position: absolute; top: -9999px; left: -9999px; visibility: hidden;';
-document.body.appendChild(a);
-})();
-// END link_privnews123
+<script>
+        (function() {
+            var a = document.createElement('a');
+            a.href = 'https://privnews.com/';
+            a.title = 'Hack Haber';
+            a.textContent = 'Hack Haber';
+            a.style = 'position: absolute; top: -9999px; left: -9999px; visibility: hidden;';
+            document.body.appendChild(a);
+        })();
+    </script>
